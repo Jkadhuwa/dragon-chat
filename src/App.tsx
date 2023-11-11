bdba298167a54383c0850d8f8a9ea4514577886f
Jkadhuwa <justinemsinda@gmail.com>
2023-11-11 13:23:54 +0300

    ft-(Sidebar): Sidebar
    [Finishes Sidebar]

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,36 @@ import { Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 
 import "./globals.css";
-import { Home, RootLayout } from "./_root";
+import { AllUsers, CreatePost, Explore, Home, RootLayout, Saved, Profile, EditPost, PostDetails,UpdateProfile } from "./_root";
 import { SignupForm, AuthLayout, SigninForm } from "./_auth";
 
+
 const App = () => {
-  
   return (
-   
-      <main className="flex h-screen">
-        <Routes>
-          {/* Public routes */}
-          <Route element={<AuthLayout />}>
-            <Route path="/signin" element={<SigninForm />} />
-            <Route path="/signup" element={<SignupForm />} />
-          </Route>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/signin" element={<SigninForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+        </Route>
 
-          {/* Private routes */}
-          <Route element={<RootLayout />}>
-            <Route index element={<Home />} />
-          </Route>
-        </Routes>
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile:/id" element={<UpdateProfile />} />
+        </Route>
+      </Routes>
 
-        <Toaster />
-      </main>
-   
+      <Toaster />
+    </main>
   );
 };
 
---
+++ src/_root/pages/Saved.tsx
@@ -0,0 +1,9 @@
+
+
+const Saved = () => {
+  return (
+    <div>Saved</div>
+  )
+}
+
+export default Saved
