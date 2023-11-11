06741244601f25a7a8941d87449129ea5067369b
Jkadhuwa <justinemsinda@gmail.com>
2023-11-11 13:56:02 +0300

    ft-(Navigation): Mobile bottombar
    [Finishes BottomBar]

---
+++ src/components/shared/BottomBar.tsx
@@ -0,0 +1,34 @@
+import { bottomBarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
+
+const BottomBar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <section className="bottom-bar">
+      {bottomBarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            className={` ${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+            key={link.label}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              className={` ${isActive && "invert-white"}`}
+              height={16}
+              width={16}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default BottomBar;
