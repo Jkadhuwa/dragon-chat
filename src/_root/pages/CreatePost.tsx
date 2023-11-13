c53c34d04a21fcc237ece360beea6f6ae4037887
Jkadhuwa <justinemsinda@gmail.com>
2023-11-13 14:02:34 +0300

    ft-(Post): create new post
    [start CreatePost]

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import PostForm from "@/components/forms/PostForm";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img
+            src="/assets/icons/add-post.svg"
+            alt="add"
+            height={36}
+            width={36}
+          />{" "}
+          <h1 className="h3-bold md:h2-bold text-left w-full">Create Post</h1>
+        </div>
+        <PostForm />
+      </div>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
